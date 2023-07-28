a0124869a612767fb86858a2f8d6be92ed5f2baa
mhkfb <19102115@ittelkom-pwt.ac.id>
2023-07-28 20:35:21 +0700

    Media farah

---
+++ story_content/user.js
@@ -0,0 +1,19 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Bx1lGFociM":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var musik = document.getElementBtId('musikFarah');
+audio.src="";
+audio.loop();
+audio.play();
+audio.volume = 0.4;
+}
+
